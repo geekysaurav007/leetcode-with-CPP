da7f1a303ada3974ef843280ad4fbe061a48bd70
Saurav Ranjan <108987930+geekysaurav007@users.noreply.github.com>
2024-09-11 11:34:18 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Subset Sums/subset-sums.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+  void func(int index,int sum,vector<int> &arr,vector<int> &subsum,int n){
+     
+      if(index==n){
+          subsum.push_back(sum);
+          return;
+      }
+      func(index+1,sum+arr[index],arr,subsum,n);
+      func(index+1,sum,arr,subsum,n);
+    
+  }
+    vector<int> subsetSums(vector<int> arr, int n) {
+       vector<int>subsum;
+       int sum=0;
+       func(0,sum ,arr,subsum,n);
+       sort(subsum.begin(),subsum.end());
+       return subsum;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin >> N;
+        vector<int> arr(N);
+        for (int i = 0; i < N; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        vector<int> ans = ob.subsetSums(arr, N);
+        sort(ans.begin(), ans.end());
+        for (auto sum : ans) {
+            cout << sum << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
