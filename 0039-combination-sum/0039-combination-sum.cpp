84dbdf9e7f7111f4537f0236f7cd2522cd78a1d1
Saurav Ranjan <108987930+geekysaurav007@users.noreply.github.com>
2024-09-18 10:13:54 +0530

    Time: 0 ms (100%), Space: 12.6 MB (81.8%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,24 +1,27 @@
 class Solution {
 public:
-    void combo(int index, vector<int>& nums, int target, vector<int>& temp,
-               vector<vector<int>>& ans) {
-        if (index == nums.size()) {
-            if (target == 0) {
-                ans.push_back(temp);
+    void combo(int index, vector<int>& nums, int &sum, int target,
+               vector<int>& ds, vector<vector<int>>& ans) {
+        int n = nums.size();
+        if(sum>target) return;
+        if (index >= n) {
+            if (sum == target) {
+                ans.push_back(ds);
             }
             return;
         }
-        if (nums[index] <= target) {
-            temp.push_back(nums[index]);
-            combo(index, nums, target - nums[index], temp, ans);
-            temp.pop_back();
-        }
-        combo(index + 1, nums, target, temp, ans);
+        ds.push_back(nums[index]);
+        sum += nums[index];
+        combo(index , nums, sum, target, ds, ans);
+        sum -= nums[index];
+        ds.pop_back();
+        combo(index + 1, nums, sum, target, ds, ans);
     }
     vector<vector<int>> combinationSum(vector<int>& nums, int target) {
         vector<int> temp;
         vector<vector<int>> ans;
-        combo(0, nums, target, temp, ans);
+        int sum = 0 ;
+        combo(0, nums, sum, target, temp, ans);
         return ans;
     }
 };
