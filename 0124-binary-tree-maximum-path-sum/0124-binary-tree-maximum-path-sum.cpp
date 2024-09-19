5d8c2c64d74909c41c3dee92228792e21ae47138
Saurav Ranjan <108987930+geekysaurav007@users.noreply.github.com>
2024-09-19 10:32:16 +0530

    Time: 17 ms (68.83%), Space: 26.4 MB (56.32%) - LeetHub

---
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int helper(TreeNode *root,int &maxsum) {
+        if (root == NULL)
+            return 0;
+        // calculate left part sum ?// max used because if any node has negative value,so just take 0
+        int leftsum =  max(0,helper(root->left, maxsum));
+         // calculate left part sum // max used because if any node has negative value,so just take 0
+        int rightsum =max(0, helper(root->right, maxsum));
+        // calculate umbrella sum -> root value+leftnode val+right node value
+        int umbrella_sum = root->val + leftsum + rightsum;
+        // calculate maximum sum upto a "X" node;
+        maxsum = max(maxsum, umbrella_sum);
+        // return the path which we have to choose-> root+left side or root+ right side
+        return root->val + max(leftsum, rightsum);
+    }
+    int maxPathSum(TreeNode* root) {
+        int maxsum = -9999999;
+        helper(root,maxsum);
+        return maxsum;
+    }
+};
