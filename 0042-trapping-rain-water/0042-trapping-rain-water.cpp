70761fa1a3551dd9e41b801e3c0feb4c6beecfc1
Saurav Ranjan <108987930+geekysaurav007@users.noreply.github.com>
2024-10-12 10:45:37 +0530

    Time: 13 ms (54.45%), Space: 26 MB (7.29%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,5 +1,18 @@
 class Solution {
 public:
+    void createPrefix(vector<int>& height, vector<int>& prefix) {
+        prefix[0] = height[0];
+        for (int i = 1; i < height.size(); i++) {
+            prefix[i] = max(prefix[i - 1], height[i]);
+        }
+    }
+    void createSuffix(vector<int>& height, vector<int>& suffix) {
+        int n = height.size();
+        suffix[n - 1] = height[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            suffix[i] = max(suffix[i + 1], height[i]);
+        }
+    }
     int trap(vector<int>& height) {
         int n = height.size();
         if (n == 0)
@@ -7,20 +20,8 @@ public:
 
         vector<int> prefix(n);
         vector<int> suffix(n);
-
-        // Fill left array
-        prefix[0] = height[0];
-        for (int i = 1; i < n; i++) {
-            prefix[i] = max(prefix[i - 1], height[i]);
-        }
-
-        // Fill right array
-        suffix[n - 1] = height[n - 1];
-        for (int i = n - 2; i >= 0; i--) {
-            suffix[i] = max(suffix[i + 1], height[i]);
-        }
-
-        // Calculate trapped water
+        createPrefix(height,prefix);
+        createSuffix(height,suffix);
         int trappedWater = 0;
         for (int i = 0; i < n; i++) {
             trappedWater += min(prefix[i], suffix[i]) - height[i];
