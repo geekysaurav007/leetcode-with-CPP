36351c249a32c4943401e77b710660a06464b2b3
Saurav Ranjan <108987930+geekysaurav007@users.noreply.github.com>
2024-10-30 21:35:36 +0530

    Time: 3 ms (69.45%), Space: 18.4 MB (68.89%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -11,8 +11,7 @@
 class Solution {
 public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        // if(lists.size()==0) return NULL;
-        // if(lists.size()==1 && lists[0]==NULL) return NULL;
+    
         auto cmp = [](ListNode* a, ListNode* b) { return a->val > b->val; };
         priority_queue<ListNode*, vector<ListNode*>, decltype(cmp)> pq;
         for (int i = 0; i < lists.size(); i++) {
