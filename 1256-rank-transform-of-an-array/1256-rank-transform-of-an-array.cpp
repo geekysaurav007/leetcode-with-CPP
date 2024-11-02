425aa2c78aea0e7cce7e38c349ff66343bf8e118
Saurav Ranjan <108987930+geekysaurav007@users.noreply.github.com>
2024-11-02 13:22:25 +0530

    Time: 47 ms (49.1%), Space: 39.1 MB (89.44%) - LeetHub

--- 1256-rank-transform-of-an-array/1256-rank-transform-of-an-array.cpp
+++ 1256-rank-transform-of-an-array/1256-rank-transform-of-an-array.cpp
@@ -1,16 +1,26 @@
 class Solution {
 public:
     vector<int> arrayRankTransform(vector<int>& arr) {
-        set<int> ele(arr.begin(),arr.end());
-        unordered_map<int,int> mp;
-        int rank=1;
-        vector<int> res;
-        for(auto s:ele){
-            mp[s]=rank++;
+        vector<int> ans(arr.size(), 0);
+        priority_queue<pair<int, int>, vector<pair<int, int>>,
+                       greater<pair<int, int>>>
+            pq;
+        for (int i = 0; i < arr.size(); i++) {
+            pq.push({arr[i], i});
         }
-        for (int i=0;i<arr.size();i++){
-            res.push_back(mp[arr[i]]);
+        int rank = 1;
+        int prev = INT_MAX;
+        while (!pq.empty()) {
+            pair<int, int> p = pq.top();
+            pq.pop();
+            if (prev == p.first) {
+                ans[p.second] = rank-1;
+                prev = p.first;
+            } else {
+                ans[p.second] = rank++;
+                prev = p.first;
+            }
         }
-        return res;
+        return ans;
     }
 };
