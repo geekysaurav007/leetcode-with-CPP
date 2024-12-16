4cdd46a246a10d53642767b1206e237329ab764e
Saurav Ranjan <108987930+geekysaurav007@users.noreply.github.com>
2024-12-16 14:16:20 +0530

    Time: 39 ms (93.55%), Space: 94.1 MB (31.18%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
@@ -10,6 +10,7 @@ public:
         int cnt = 0;
         for (int i = 1; i < intervals.size(); i++) {
             if (intervals[i][0] < temp[1]) {
+             
                 cnt++;
             } else {
                 temp = intervals[i];
