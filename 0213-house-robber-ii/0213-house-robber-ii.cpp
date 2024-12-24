09ded388dde7757261329ac8d3034c1a4e6503a4
Saurav Ranjan <108987930+geekysaurav007@users.noreply.github.com>
2024-12-24 12:34:42 +0530

    Time: 0 ms (100%), Space: 10.2 MB (36.14%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
-    int solve(int index, vector<int>& nums, int n, vector<int>& dp) {
+    int dp[101];
+    int solve(int index, vector<int>& nums, int n) {
         if (index > n) {
             return 0;
         }
         if (dp[index] != -1) {
             return dp[index];
         }
-        int pick = nums[index] + solve(index + 2, nums, n, dp);
-        int non_pick = 0 + solve(index + 1, nums, n, dp);
+        int pick = nums[index] + solve(index + 2, nums, n);
+        int non_pick = 0 + solve(index + 1, nums, n);
         dp[index] = max(pick, non_pick);
         return dp[index];
     }
@@ -18,10 +19,10 @@ public:
             return nums[0];
         if (n == 2)
             return max(nums[0], nums[1]);
-        vector<int> dp(n + 1, -1);
-        int pick_zero = solve(0, nums, n - 2, dp);
-        vector<int> dp2(n + 1, -1);
-        int pick_one = solve(1, nums, n - 1, dp2);
+        memset(dp, -1, sizeof(dp));
+        int pick_zero = solve(0, nums, n - 2);
+        memset(dp, -1, sizeof(dp));
+        int pick_one = solve(1, nums, n - 1);
         return max(pick_zero, pick_one);
     }
 };
