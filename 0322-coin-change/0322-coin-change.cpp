8886fa42c328e2aa0d5a741b75b672d43a4659bd
Saurav Ranjan <108987930+geekysaurav007@users.noreply.github.com>
2024-12-29 13:49:39 +0530

    Time: 66 ms (12.61%), Space: 50 MB (17.43%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int solve(int index, vector<int>& nums, long long sum, int target, int cnt,
+    int solve(int index, vector<int>& nums, long long sum, int target,
               vector<vector<int>>& dp) {
         if (sum > target || index >= nums.size()) {
             return (sum == target) ? 0 : INT_MAX;
@@ -8,18 +8,18 @@ public:
         if (dp[index][sum] != -1)
             return dp[index][sum];
         long long take =
-            solve(index, nums, sum + nums[index], target, cnt + 1, dp);
+            solve(index, nums, sum + nums[index], target, dp);
         if (take != INT_MAX) {
             ++take;
         }
-        long long not_take = solve(index + 1, nums, sum, target, cnt, dp);
+        long long not_take = solve(index + 1, nums, sum, target, dp);
 
         dp[index][sum] = min(take, not_take);
         return dp[index][sum];
     }
     int coinChange(vector<int>& coins, int amount) {
         vector<vector<int>> dp(coins.size(), vector<int>(amount + 1, -1));
-        long long result = solve(0, coins, 0, amount, 0, dp);
+        long long result = solve(0, coins, 0, amount, dp);
         return result == INT_MAX ? -1 : result;
     }
 };
