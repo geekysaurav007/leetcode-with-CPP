f66baa230eb62857a1c21435eeac307011e460d6
Saurav Ranjan <108987930+geekysaurav007@users.noreply.github.com>
2025-04-10 16:50:17 +0530

    Time: 0 ms (100%), Space: 9.2 MB (34.79%) - LeetHub

--- 1993-sum-of-all-subset-xor-totals/1993-sum-of-all-subset-xor-totals.cpp
+++ 1993-sum-of-all-subset-xor-totals/1993-sum-of-all-subset-xor-totals.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     int totalSum = 0;
-     void solve(int index, vector<int>& nums, int sum) {
+    int solve(int index, vector<int>& nums, int sum) {
         if (index >= nums.size()) {
-            totalSum += sum;
-            return;
+            return sum;
         }
-        solve(index + 1, nums, nums[index] ^ sum);
-        solve(index + 1, nums, sum);
+        int take = solve(index + 1, nums, nums[index] ^ sum);
+        int skip = solve(index + 1, nums, sum);
+        return take + skip;
     }
     int subsetXORSum(vector<int>& nums) {
-        solve(0, nums, 0);
-        return totalSum;
+        return solve(0, nums, 0);
+        // return totalSum;
     }
 };
