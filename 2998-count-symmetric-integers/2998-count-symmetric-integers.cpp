b40995a8f3731e5bf2a56e3b245326c9c6d842c1
Saurav Ranjan <108987930+geekysaurav007@users.noreply.github.com>
2025-04-11 20:45:20 +0530

    Time: 83 ms (23.88%), Space: 10 MB (39.51%) - LeetHub

--- 2998-count-symmetric-integers/2998-count-symmetric-integers.cpp
+++ 2998-count-symmetric-integers/2998-count-symmetric-integers.cpp
@@ -12,12 +12,14 @@ public:
         for (int i = low; i <= high; i++) {
             string n = to_string(i);
             int l = n.length();
-            if (n.length() % 2 == 0) {
-                int left = check(0, l / 2 - 1, n);
-                int right = check(l / 2, l - 1, n);
-                if (left == right) {
-                    cnt++;
-                }
+            if (n.length() % 2 == 0 &&
+                    check(0, l / 2 - 1, n) == check(l / 2, l - 1, n)) {
+                // int left = check(0, l / 2 - 1, n);
+                // int right = check(l / 2, l - 1, n);
+                // if (left == right) {
+                //     cnt++;
+                // }
+                cnt++;
             }
         }
         return cnt;
